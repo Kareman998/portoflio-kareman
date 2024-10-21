98a7311c7673612ea9d0ef55a80f870108132ab1
KaremanGhanem <kareman.ghanem@casivu.com>
2024-10-21 07:26:17 +0300

    new changes

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,14 +1,32 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAKBORqvwwoVPavQjWYRBsIiKsPpnAzMDI",
+//   authDomain: "portfolio-3d62f.firebaseapp.com",
+//   projectId: "portfolio-3d62f",
+//   storageBucket: "portfolio-3d62f.appspot.com",
+//   messagingSenderId: "174398431968",
+//   appId: "1:174398431968:web:ba76399a67746f87776685",
+//   measurementId: "G-J9L99MMQC0"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+
 import { initializeApp } from "firebase/app";
+
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAKBORqvwwoVPavQjWYRBsIiKsPpnAzMDI",
-  authDomain: "portfolio-3d62f.firebaseapp.com",
-  projectId: "portfolio-3d62f",
-  storageBucket: "portfolio-3d62f.appspot.com",
-  messagingSenderId: "174398431968",
-  appId: "1:174398431968:web:ba76399a67746f87776685",
-  measurementId: "G-J9L99MMQC0"
+  apiKey: "AIzaSyDPxjwC-NRIDqN5zCUSEGWB82PiN0eaifM",
+  authDomain: "portoflio-dd09d.firebaseapp.com",
+  projectId: "portoflio-dd09d",
+  storageBucket: "portoflio-dd09d.appspot.com",
+  messagingSenderId: "385967089625",
+  appId: "1:385967089625:web:2dacd9a1323d8e4202dbc8",
+  measurementId: "G-FYYL7N7Y6X",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- src/User.tsx
+++ src/User.tsx
@@ -1,150 +1,206 @@
 import { IconBrandGithub, IconBrandInstagram, IconBrandLeetcode, IconBrandLinkedin, IconBrandYoutube } from "@tabler/icons-react";
+import JumiaImage from "./images/Ecommerce jumia.png";
+import Linkedin from "./images/linkedin.png";
+import Shopping from "./images/shopping.png";
+import Casivu from "./images/chart3.png";
+import jumiaDashboard from "./images/jumiadashboard.png";
+import Movieapp from "./images/movie.png";
+
 const Info = {
-    name: "Chandrabhan Maurya",
-    stack: ["Software Engineer", "Full Stack Developer", "Competitive Programmer", "Freelancer", "Open Source Contributor"],
-    bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!"
-}
-
-
+  name: "Kareman Ahmed",
+  stack: ["Software Engineer", "Full Stack MEARN Developer", "Freelancer"],
+  bio: "I'm a passionate software engineer specialized in crafting efficient and scalable solutions, deliver top-notch web applications, and provide freelance services that exceed client expectations. Let's connect and create something amazing together!",
+};
 
 const ProjectInfo = [
-    {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
-        live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
-    },
-    {
-        title: "Facebook Clone",
-        desc: "Facebook Clone is a full-stack web application that replicates the core features of Facebook, including user authentication, posting, and commenting. The front end is built with React, TailwinCSS, Tabler-Icons, Mantine and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, post updates, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Facebook Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Facebook.png",
-        live: false,
-        technologies: ["React", "Springboot", "MongoDB", "Tailwind", "Tabler-Icons", "Firebase"],
-        link: "https://github.com/Code-Mars/Facebook-Clone",
-        github: "https://github.com/Code-Mars/Facebook-Clone"
-    },
-    {
-        title: "Spotify Clone",
-        desc: "Spotify Clone is a full-stack web application that replicates the core features of Spotify, including user authentication, music playback, and playlist creation. The front end is built with React, TailwindCSS, Heroicons, and Firebase, while the back end uses Firebase for user authentication and data storage. Users can create an account, log in, search for songs, and create playlists. The app features real-time updates, so users can see new songs as they are added. Spotify Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Spotify.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Tabler-Icons", "MongoDB", "Firebase"],
-        link: "https://github.com/Code-Mars/Spotify-Clone",
-        github: "https://github.com/Code-Mars/Spotify-Clone"
-    },
-    {
-        title: "Travel Tracker",
-        desc: "Travel Tracker is a full-stack web application that allows users to track their travel experiences and share them with others. The front end is built with React, Bootstrap, while the back end uses Node.js, Express, and PostgresSQL. Users can create an account, log in, add new trips, and view their past trips on a map. The app features a clean, modern design with interactive maps and a user-friendly interface. Travel Tracker combines functionality with style, making it easy and enjoyable to record and share travel memories.",
-        image: "Travel.png",
-        live: false,
-        technologies: ["React", "Node.js", "PostgresSQL", "Express", "Bootstrap"],
-        link: "https://github.com/Code-Mars/Travel-Tracker",
-        github: "https://github.com/Code-Mars/Travel-Tracker"
-    },
-    {
-        title: "Instagram Clone",
-        desc: "Instagram Clone is a full-stack web application that replicates the core features of Instagram, including user authentication, image uploading, and commenting. The front end is built with React, TailwindCSS, and Heroicons, while the back end uses Firebase for user authentication and image storage. Users can create an account, log in, upload images, and comment on posts. The app features real-time updates, so users can see new comments as they are posted. Instagram Clone provides a seamless user experience with a clean, modern design and intuitive functionality.",
-        image: "Instagram.png",
-        live: false,
-        technologies: ["React", "Springboot", "Tailwind", "Heroicons", "Firebase", "MongoDB"],
-        link: "https://github.com/Code-Mars/Instagram-Clone",
-        github: "https://github.com/Code-Mars/Instagram-Clone"
-    },
-    {
-        title: "CodeX Code Editor",
-        desc: "CodeX is a modern, fully responsive code editor built using React, TailwindCSS, and Ace Editor, offering a seamless coding experience across all devices. It features syntax highlighting for popular programming languages, including HTML, CSS, JavaScript, Python, and Java. Users can create, edit, and save code files, with the option to download them as text files. CodeX combines robust functionality with a sleek, intuitive design, making coding effortless and enjoyable.",
-        image: "CodeX.png",
-        live: false,
-        technologies: ["React", "Tailwind", "Ace Editor"],
-        link: "https://github.com/Code-Mars/CodeX",
-        github: "https://github.com/Code-Mars/CodeX"
-    }
-]
+  {
+    title: "Dashboard E-comm App",
+    desc: "Led the creation of an ecommerce platform from scratch, using React, Vite, and Firebase.Introduced TypeScript into the project and assisted colleagues in adopting it, enhancing code reliabilityand developer productivity.Worked closely with teammates to design and build smooth, fast features for users.",
+    image: Casivu,
+    live: false,
+    technologies: [
+      "Vite",
+      "Firebase",
+      "Tailwindcss",
+      "CloudFunction",
+      "ShadCN",
 
+      "SendGrid",
+    ],
+    // link: "https://github.com/Code-Mars/Travel-Tracker",
+    // github: "https://github.com/Code-Mars/Travel-Tracker",
+  },
+  {
+    title: "Jumia E-comm App",
+    desc: "Jumia is a modern, fully responsive e-commerce platform built using Angular, CSS,  and Bootstrap,NodeJS,MongoDB offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
+    image: JumiaImage,
+    live: true,
+    technologies: ["Angular", "CSS", "Redux", "NodeJs", "MongoDB"],
+    link: "https://jumia-clint.netlify.app/Home",
+    github: "https://github.com/Kareman998/jumiaclone",
+  },
 
-const SkillInfo = [
-    {
-        title: "Frontend",
-        skills: ["HTML", "CSS", "SASS", "JavaScript", "React JS", "Angular", "Redux", "Tailwind CSS", "GSAP", "Material UI", "Bootstrap"
-        ]
-    },
-    {
-        title: "Backend",
-        skills: ["Springboot", "Node JS", "Express JS", "MySQL", "MongoDB", "Firebase", "PostgresSQL"]
-    },
-    {
-        title: "Languages",
-        skills: ["C", "C++", "Java", "JavaScript", "TypeScript"]
-    },
-    {
-        title: "Tools",
-        skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass", "Spring Tool Suite"]
-    }
-]
-const socialLinks = [
-    { link: "https://github.com/Code-Mars", icon: IconBrandGithub },
-    { link: "https://www.linkedin.com/in/chandrabhan-maurya", icon: IconBrandLinkedin },
-    { link: "https://www.instagram.com/code.marshal_", icon: IconBrandInstagram }, 
-    { link: "https://www.youtube.com/channel/UC1ki6jaFvFiH_E79b9FYptw", icon: IconBrandYoutube },
-    { link: "https://www.leetcode.com/u/CodeMars", icon: IconBrandLeetcode }
+  {
+    title: "Shopping E-comm App",
+    desc: "Shopping is a modern, fully responsive e-commerce platform built using ReactJs, CSS, NodeJs, React-Bootstrap,Sokit Oi  and Bootstrap,NodeJS,MongoDB offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
+    image: Shopping,
+    live: false,
+    technologies: ["React", "React-Bootstrap", "MongoDB", "NodeJs,Sokit Oi"],
+    link: "https://github.com/Kareman998/Shopping-Client-Side",
+    github: "https://github.com/Kareman998/Shopping-Client-Side",
+  },
+  {
+    title: "Jumia Dashboard",
+    desc: "The Jumia Dashboard app is a comprehensive web application designed to provide an intuitive and user-friendly interface for managing and analyzing e-commerce data. Built using React for the frontend, this app offers a responsive and dynamic user experience enhanced by Bootstrap for seamless styling.On the backend, Node.js and Express work together to create a robust server environment, ensuring efficient data handling and API management. The application utilizes MongoDB as its database,",
+    image: jumiaDashboard,
+
+    live: false,
+    technologies: ["React", "NodeJs", "Bootstrap", "Express", "MongoDB"],
+    link: "https://github.com/JECDB-PROJECT/jumia-dashboard",
+    github: "https://github.com/JECDB-PROJECT/jumia-dashboard",
+  },
+  {
+    title: "Movie App",
+    desc: "MovieApp is a dynamic web application designed to provide movie enthusiasts with an engaging platform to discover, explore, and manage their favorite films. Developed using React, this app delivers a seamless user experience, enhanced by the responsive design capabilities of Bootstrap and custom styling with CSS.Utilizing Redux for state management, MovieApp ensures efficient handling of application data, allowing users to easily navigate through various movie categories, view detailed information, and maintain a personalized watchlist. The app interacts with a fake API to simulate real-world data retrieval, offering a realistic browsing experience without the need for a backend server.",
+    image: Movieapp,
+    live: false,
+    technologies: ["React", "Bootstap", "Redux"],
+    link: "https://github.com/Kareman998/Movieapp?tab=readme-ov-file",
+    github: "https://github.com/Kareman998/Movieapp?tab=readme-ov-file",
+  },
+  {
+    title: "LinkedIn Clone",
+    desc: "LinkedIn, including user profiles, job postings, and networking functionalities. Built using HTML5, CSS3, and JavaScript, this clone allows users to create accounts, update profiles, and connect with others. The site is optimized for various devices, ensuring a seamless user experience on both desktop and mobile platforms. It provides an intuitive interface for managing job postings, I created two pages, the first is the home page and the second is the profile page.",
+    image: Linkedin,
+    live: true,
+    technologies: ["JavaScript", "HTML5", "CSS3"],
+    link: "https://kareman998.github.io/Linkedindesign/",
+    github: "https://github.com/Kareman998/Linkedindesign",
+  },
 ];
 
+const SkillInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      "HTML",
+      "CSS",
+      "SASS",
+      "JavaScript",
+      "React JS",
+      "Angular",
+      "Redux",
+      "Bootstrap",
+      "Redux Toolkit",
+      "Tailwind CSS",
+      "ShadCN",
+      "Material UI",
+      "Bootstrap",
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      "Nest JS",
+      "Node JS",
+      "Express JS",
+      "MySQL",
+      "MongoDB",
+      "Firebase",
+    ],
+  },
+  {
+    title: "Languages",
+    skills: ["C", "C++", "JavaScript", "TypeScript"],
+  },
+  {
+    title: "Tools",
+    skills: ["Git", "Github", "VS Code", "Postman", "MongoDB Compass"],
+  },
+];
+const socialLinks = [
+  { link: "https://github.com/Kareman998", icon: IconBrandGithub },
+  {
+    link: "https://www.linkedin.com/in/kareman-ahmed-a5b782221?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    icon: IconBrandLinkedin,
+  },
+  {
+    link: "https://www.instagram.com/kareman.fadel?igsh=YTEwZ3VjajJjdDQz&utm_source=qr",
+    icon: IconBrandInstagram,
+  },
+  {
+    link: "https://youtube.com/@kareman998?si=cAkJxswiToYnlsV6",
+    icon: IconBrandYoutube,
+  },
+  { link: "https://leetcode.com/profile/", icon: IconBrandLeetcode },
+];
 
 const ExperienceInfo = [
-    {
-        role: "Specialist Programmer",
-        company: "Infosys",
-        date: "Oct 2023 - Present",
-        desc: "I led software development with Spring Boot, React, and Angular, creating scalable microservices and interfaces. I optimized performance by integrating frontend and backend, and enhanced security with RESTful APIs. Collaborating in agile teams, I fostered innovation and efficiency for top-quality solutions.",
-        skills: ["Springboot", "React JS", "Angular", "Node JS", "MySQL", "MongoDB", "Microservices"]
-    },
-    {
-        role: "System Engineer",
-        company: "Infosys",
-        date: "Nov 2022 - Sep 2023",
-        desc: " I leveraged my skills in Java, Selenium, automation testing, and MySQL to design and implement automated testing frameworks, ensuring robust software validation. I optimized database performance and reliability, and collaborated with cross-functional teams to enhance system operations and ensure seamless integration.",
-        skills: ["Java", "MySQL", "Hibernate", "Selenium", "Jenkins", "JIRA", "Automation Testing"]
-    }
-]
+  {
+    role: "Front End Developer",
+    // image: "Movieapp",
+    company: "Casivu",
+    date: "Oct 2023 - Present",
+    desc: "Led the creation of an ecommerce platform from scratch, using React, Vite, and Firebase. Introduced TypeScript into the project and assisted colleagues in adopting it, enhancing code reliabilityand developer productivity. Worked closely with teammates to design and build smooth, fast features for users",
+    skills: [
+      "React JS",
+      "Firebase",
+      "Tailwindcss",
+      "Matrial Ui",
+      "Vite",
+      "SendGrid",
+      "ShadCN",
+      "jest",
+      "testing library",
+    ],
+  },
+  {
+    role: "Instructor",
+    company: "Information Technology Institute (ITI) ",
+    date: "March 2024 - Present",
+    desc: " Responsible for teaching and mentoring students in web development and software engineering using technologies like WordPress, MySQL, Angular, JavaScript, ReactJS, NodeJS, and MongoDB, with a focus on hands-on experience and industry-relevant skills.",
+    skills: [
+      "Wordpress",
+      "MySQL",
+      "Angular",
+      "Javascript",
+      "ReactJs",
+      "NodeJs",
+      "Testing",
+      "MongoDB",
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+    ],
+  },
+];
 const Slugs = [
-    "typescript",
-    "spring",
-    "javascript",
-    "dart",
-    "java",
-    "react",
-    "angular",
-    "flutter",
-    "android",
-    "html5",
-    "css3",
-    "springboot",
-    "mongodb",
-    "selenium",
-    "nodedotjs",
-    "express",
-    "nextdotjs",
-    "prisma",
-    "mysql",
-    "amazonaws",
-    "postgresql",
-    "firebase",
-    "nginx",
-    "vercel",
-    "testinglibrary",
-    "jest",
-    "cypress",
-    "docker",
-    "git",
-    "jira",
-    "github",
-    "gitlab",
-    "visualstudiocode",
-    "androidstudio",
-    "sonarqube",
-    "figma",
+  "typescript",
+  "spring",
+  "javascript",
+  "react",
+  "angular",
+  "nodejs",
+  "html5",
+  "css3",
+  "sass",
+  "mongodb",
+  "selenium",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "mysql",
+  "firebase",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "figma",
 ];
 export { Info, ProjectInfo,socialLinks, SkillInfo, ExperienceInfo, Slugs };
